b99e593a6ac8db2015edb0f6d3d5602458814508
TS-Vikas <vikas.vadhiya@techstaunch.com>
2026-01-27 14:38:59 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home";
+import NotFound from "./pages/NotFound";
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
